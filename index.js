8348d4ffb5238f6c6546da37824abb27b09e89de
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-11-15 20:59:53 +0100

    add password hide/show icon

--- index.js
+++ index.js
@@ -30,10 +30,31 @@ signupForm = document.querySelector('.signupForm'),
 signupError = document.querySelector('.signupError'),
 signinError = document.querySelector('.signinError');
 
+const formOpenBtn = document.querySelector("#form-open"),
+  home = document.querySelector(".home"),
+  formContainer = document.querySelector(".form_container"),
+  formCloseBtn = document.querySelector(".form_close"),
+  signup = document.querySelector("#signup"),
+  login = document.querySelector("#login"),
+  pwShowHide = document.querySelectorAll(".pw_hide");
 
+  formOpenBtn.addEventListener("click", () => home.classList.add("show"));
+formCloseBtn.addEventListener("click", () => home.classList.remove("show"));
 
-//main
-const formContainer = document.querySelector(".form_container");
+pwShowHide.forEach((icon) => {
+    icon.addEventListener("click", () => {
+      let getPwInput = icon.parentElement.querySelector("input");
+      if (getPwInput.type === "password") {
+        getPwInput.type = "text";
+        icon.classList.replace("uil-eye-slash", "uil-eye");
+      } else {
+        getPwInput.type = "password";
+        icon.classList.replace("uil-eye", "uil-eye-slash");
+      }
+    });
+  });
+  
+
 
 // clear signin&signup error
 pass.addEventListener('keyup', ()=>{
