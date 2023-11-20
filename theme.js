91f75bd5e932d6b3e90e9c63146547eeae6eecf7
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-11-20 07:13:24 +0100

    add script to open/close theme modal

--- theme.js
+++ theme.js
@@ -7,3 +7,25 @@ const priColor = document.querySelectorAll('.choose-bg div');
 const createBtn = document.querySelector('.create .btn');
 const creatPost = document.querySelector('.newPost');
 const signout = document.querySelector('.signoutBtn');
+
+//Create Post
+createBtn.addEventListener('click', () => {
+    creatPost.style.display = 'flex';
+})
+
+//Signout
+signout.addEventListener('click',()=>{
+    localStorage.removeItem("fullname");
+    localStorage.removeItem("Userid");
+    window.location.href = "./index.html";
+})
+// close Modal
+themeModal.addEventListener('click', e =>{
+    if(e.target.classList.contains('customize-theme')){
+        themeModal.style.display = "none";
+    }
+})
+//open Modal
+theme.addEventListener('click', () => {
+    themeModal.style.display = 'grid';
+});
