51c54282c27ca3e401736d7491e1319b0f1363a0
sodiqbinaliu <sodiboy4jamb@gmail.com>
2023-11-22 07:13:11 +0100

    display post in feed box

--- post.js
+++ post.js
@@ -23,6 +23,7 @@ const linkDesc = document.querySelector("#linkDesc");
 const postBtn = document.querySelector(".newPost");
 const feeds = document.querySelector('.feeds');
 
+//create new post
 const addPost = async (userId,fullname) => {
     try {
         const docRef = await addDoc(collection(db, "posts"), {
@@ -39,3 +40,50 @@ const addPost = async (userId,fullname) => {
         console.error("Error adding document: ", e);
       }
 }
+
+ //submit new post
+postBtn.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const userId = localStorage.getItem("userId");
+    const fullname = localStorage.getItem("fullname");
+    addPost(userId, fullname);
+    creatPost.style.display = 'none';
+    link.value = "";
+    linkDesc.value = "";
+    alert('Post Successful: Kindly wait for approval');
+})
+
+//display all post in feed area
+const addpost = (doc)=>{
+    let post = `
+    <div class="feed">
+    <div class="head">
+        <div class="user">
+            <div class="info">
+                <h3>${doc.fullname}</h3>
+                <small>${doc.timestamp.toDate()}</small>
+            </div>
+        </div>    
+    </div>
+    <div class="post">
+        <div class="link">
+            ${doc.link}
+        </div><br><br><br>
+        <div class="desc">
+         ${doc.desc}
+        </div>
+    </div>
+    </div>
+  `
+    feeds.innerHTML += post;
+  }
+
+  // to display only approve post
+  const check = query(collection(db, "posts"), where('approved','==',true));
+  
+  async ()=>{
+    const snapshot = await getDocs(check);    
+    snapshot.docs.forEach((doc)=>{
+    addpost(doc.data());
+})
+  }
